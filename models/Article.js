b415fb24a2e81b749a038f8891421df2ee28654a
Jenny <jennyelisa07@aol.com>
2019-08-19 20:24:46 -0400

    note, index, and article.js created. logic added to include article summary

--- models/Article.js
+++ models/Article.js
@@ -1,2 +1,25 @@
 var mongoose = require("mongoose");
-var Schema = 
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+    title: {
+        type: String,
+        required: tru
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: String,
+        required: true
+    },
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
